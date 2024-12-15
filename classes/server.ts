a4b8275e24eb5835678cf3f8ba91dee6f7c3797c
max.poblete2905 <max.poblete2905@gmail.com>
2024-12-15 02:33:54 -0300

    config socket io

--- classes/server.ts
+++ classes/server.ts
@@ -1,17 +1,52 @@
-import express from 'express'
-import { SERVER_PORT } from '../global/environment';
+import express from "express";
+import { SERVER_PORT } from "../global/environment";
+import { Server as socketIOServer } from "socket.io"; // Importación corregida
+import http from "http";
+import { Client } from "socket.io/dist/client";
+import { simpleLogger } from "../config/logger";
 
 export default class Server {
+  private static _intance: Server;
+  private httpServer: http.Server;
+  public app: express.Application;
+  public port: number;
+  public io: socketIOServer;
 
-    public app: express.Application;
-    public port: number;
+  private constructor() {
+    this.app = express();
+    this.port = SERVER_PORT;
+    this.httpServer = new http.Server(this.app);
+    this.io = new socketIOServer(this.httpServer, {
+      cors: {
+        origin: "http://localhost:4200", // Permite el origen de tu frontend
+        methods: ["GET", "POST"], // Métodos permitidos
+        allowedHeaders: ["Content-Type", "Authorization"], // Encabezados permitidos
+        credentials: true, // Permite credenciales (cookies, autorización, etc.)
+      },
+    });
 
-    constructor() {
-        this.app = express();
-        this.port = SERVER_PORT
-    }
+    this.onSocket();
+  }
 
-    start(callback: () => void) {
-        this.app.listen(this.port, callback)
-    }
+  public static get intace() {
+    return this._intance || (this._intance = new this());
+  }
+
+  onSocket() {
+    simpleLogger.info("Escuchando conexión -- socket");
+
+    this.io.on("connection", (client) => {
+      // Corregido el nombre de 'conennction' a 'connection'
+      simpleLogger.info("Cliente conectado");
+
+      // Aquí puedes manejar eventos del cliente, como 'disconnect', 'message', etc.
+      client.on("disconnect", () => {
+        simpleLogger.info("Cliente desconectado");
+      });
+    });
+  }
+
+  start(callback: () => void) {
+    this.httpServer.listen(this.port, callback);
+  }
 }
--- index.ts
+++ index.ts
@@ -1,17 +1,24 @@
 import Server from "./classes/server";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 import router from "./routes/router";
+import cors from "cors";
+import dotenv from "dotenv";
 import { requestLogger } from "./middlewares/requestLogger";
 import { simpleLogger } from "./config/logger";
+import { PREFIX } from "./global/environment";
 
-const server = new Server();
+const server = Server.intace;
+dotenv.config();
 
-// Middlewares
+// Habilitar CORS para el frontend en http://localhost:4200
+server.app.use(cors({ origin: "http://localhost:4200", credentials: true }));
 server.app.use(bodyParser.urlencoded({ extended: true }));
 server.app.use(bodyParser.json());
-server.app.use(requestLogger); // Agregamos el middleware de logging
-server.app.use('/', router);
+server.app.use(requestLogger);
 
+server.app.use(PREFIX, router);
+
+// Iniciar el servidor
 server.start(() => {
-    simpleLogger.info(`Servidor corriendo en el puerto ${server.port}`);
+  simpleLogger.info(`Servidor corriendo en el puerto ${server.port}`);
 });
